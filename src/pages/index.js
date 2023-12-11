0000b13955639251c09334e88aa933ec6e0bd8fc
ReactedTitan880 <thomasnardou@icloud.com>
2023-12-12 00:09:37 +0100

    add language table

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,63 @@
+import { FaHtml5, FaPhp, FaUnity } from "react-icons/fa";
+import { IoLogoCss3, IoLogoNodejs } from "react-icons/io";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiCsharp, SiPowershell, SiVisualstudiocode, SiVisualstudio, SiGodotengine } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+
+const ICON_SIZE = 80;
+
 export default function Home() {
   return (
     <>
       <div className="aboutMe">
-            <div className="description">
-                <h2>About Me :</h2>
-            <p>
-                My name is Thomas Nardou, and I'm currently an apprentice developer in Switzerland. 
-                My passion for the world of computing is reflected in my constant commitment to explore the many facets of programming. 
-                As a fervent advocate of digital creativity, I also thrive in the field of graphic design, transforming ideas into captivating visuals. 
-                I'm also an avid fan of the video game minecraft.
-            </p>
-            </div>
-            <div className="pfp"></div>
+        <div className="description">
+          <h2>About Me :</h2>
+          <p>
+            My name is Thomas Nardou, and I'm currently an apprentice developer in Switzerland.
+            My passion for the world of computing is reflected in my constant commitment to explore the many facets of programming.
+            As a fervent advocate of digital creativity, I also thrive in the field of graphic design, transforming ideas into captivating visuals.
+            I'm also an avid fan of the video game minecraft.
+          </p>
         </div>
+        <div className="pfp"></div>
+      </div>
 
-        <div className="language">
-            <table>
-                
-            </table>
-        </div>
+      <div className="language">
+        <table>
+          <thead>
+            <tr>
+              <th>Language</th>
+              <th>Front-End</th>
+              <th>Back-End</th>
+              <th>Tools</th>
+            </tr>
+          </thead>
+          <tbody className="tableBody">
+            <tr>
+              <th>
+                <IoLogoJavascript size={ICON_SIZE} className="icon" />
+                <SiCsharp size={ICON_SIZE} className="icon" />
+                <SiPowershell size={ICON_SIZE} className="icon" />
+              </th>
+              <th>
+                <FaHtml5 size={ICON_SIZE} className="icon" />
+                <IoLogoCss3 size={ICON_SIZE} className="icon" />
+              </th>
+              <th>
+                <IoLogoNodejs size={ICON_SIZE} className="icon" />
+                <FaPhp size={ICON_SIZE} className="icon" />
+                <GrMysql size={ICON_SIZE} className="icon" />
+              </th>
+              <th>
+                <SiVisualstudiocode size={ICON_SIZE} className="icon" />
+                <SiVisualstudio size={ICON_SIZE} className="icon" />
+                <SiGodotengine size={ICON_SIZE} className="icon" />
+                <FaUnity size={ICON_SIZE} className="icon" />
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }

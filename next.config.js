db544d974a85beb643d20d219f6f08fd2d1cb02e
ReactedTitan880 <thomasnardou@icloud.com>
2023-12-12 17:55:05 +0100

    try to fix the export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: 'export',
-  // reactStrictMode: true,
-
-  // basePath: '/src',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
-
+ 
 module.exports = nextConfig

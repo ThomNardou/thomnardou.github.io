7806118f93b62e993e35d647871d583476ececb4
ReactedTitan880 <thomasnardou@icloud.com>
2023-12-12 09:33:14 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
         <div className="description">
           <h2>About Me :</h2>
           <p>
-            My name is Thomas Nardou, and I'm currently an apprentice developer in Switzerland.
+            My name is Thomas Nardou, and I am currently an apprentice developer in Switzerland.
             My passion for the world of computing is reflected in my constant commitment to explore the many facets of programming.
             As a fervent advocate of digital creativity, I also thrive in the field of graphic design, transforming ideas into captivating visuals.
-            I'm also an avid fan of the video game minecraft.
+            I am also an avid fan of the video game minecraft.
           </p>
         </div>
         <div className="pfp"></div>

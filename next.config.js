5a0dc9a40fc8f5a275098967395e98162e4ce248
ReactedTitan880 <thomasnardou@icloud.com>
2023-12-12 12:36:13 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   reactStrictMode: true,
 
-  basePath: '/src',
+  // basePath: '/src',
 }
 
 module.exports = nextConfig
--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,10 @@ export default function PortfolioHeader() {
     return (
       <>
         <header>
-            <video autoPlay muted id="backgroundVideo">
+            <video autoPlay loop muted id="backgroundVideo">
               <source src="/background.mp4" type="video/mp4"></source>
             </video>
-            <h1 id="MainTitle">Welcome in my Portfolio</h1>
+            <h1 id="MainTitle">Welcome to my Portfolio</h1>
         </header>
       </>
     )
